146b38364b04b10ce086b5fb2965b6d9c25bfc6c
Mausmee <mjmausm1976@gmail.com>
2019-11-13 16:59:07 +0530

    branch

--- server.js
+++ server.js
@@ -43,6 +43,22 @@ app.put('/product', (request, response) => {
         response.send(products)
     })
 })
+app.vibha('/product', (request, response) => {
+    const connection = connect()
+    const statement = `select * from Product`
+    connection.query(statement, (error, products) => {
+        connection.end()
+        response.send(products)
+    })
+})
+app.shivani('/product', (request, response) => {
+    const connection = connect()
+    const statement = `select * from Product`
+    connection.query(statement, (error, products) => {
+        connection.end()
+        response.send(products)
+    })
+})
 app.listen(4000, () => {
     console.log(`Server started on port 4000`);
 });
